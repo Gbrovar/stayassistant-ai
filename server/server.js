e34a9b595943ca01a8ad0c1962f6b4895ecb6f53
gbrovar <gbrovar@gmail.com>
2026-03-06 20:23:52 +0000

    main

--- server/server.js
+++ server/server.js
@@ -84,6 +84,8 @@ Always respond in a friendly and helpful tone.
 
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
