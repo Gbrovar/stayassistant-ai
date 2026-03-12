c613f6f6ecf282a4ed998e5c746fa7baefe6dd98
gbrovar <gbrovar@gmail.com>
2026-03-12 14:23:45 +0000

    fix railway server config

--- public/chat.js
+++ public/chat.js
@@ -563,7 +563,7 @@ async function sendMessage(forcedText = null, displayLabel = null) {
 
         /* luego quick actions */
 
-        //await showQuickActions();
+        await showQuickActions();
 
     } catch (error) {
 
