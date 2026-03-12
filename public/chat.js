f2ba7b91b461c1b475cd9571fed9d3465a0d8ff9
gbrovar <gbrovar@gmail.com>
2026-03-12 16:15:30 +0000

    fix railway server config

--- public/chat.js
+++ public/chat.js
@@ -344,32 +344,14 @@ async function showRecommendations(text) {
 
         messages.innerHTML += `
 
-            <div class="bot-wrapper">
-
+        <div class="bot-wrapper">
             <div class="bot-avatar">📍</div>
-
             <div class="bot-message">
-
-            <b>${place.name}</b><br>
-
-            ⭐ ${place.rating}
-            ${place.open === true ? " 🟢 Open" : ""}<br>
-
-            📍 ${place.address}<br><br>
-
-            <a 
-            href="https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(place.name + " " + place.address)}"
-            target="_blank"
-            class="maps-btn"
-            >
-            Open in Google Maps
-            </a>
-
+            <b>${title}</b>
             </div>
+        </div>
 
-            </div>
-
-            `;
+        `;
 
         /* --- RENDER PLACES --- */
 
