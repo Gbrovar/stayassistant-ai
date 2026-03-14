9329bc3bad319438aa70b43a768fa7dbb69a9d5b
gbrovar <gbrovar@gmail.com>
2026-03-14 01:48:44 +0000

    fix railway server config

--- server/db/redis.js
+++ server/db/redis.js
@@ -1,5 +1,9 @@
 import { createClient } from "redis"
 
+const redis = createClient({
+  url: process.env.REDIS_URL
+})
+
 if (!process.env.REDIS_URL) {
   throw new Error("REDIS_URL not configured")
 }
