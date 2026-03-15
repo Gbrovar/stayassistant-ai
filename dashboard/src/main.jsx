28ca50ba4485cfc6fb6f15af4f9e127ee9563522
gbrovar <gbrovar@gmail.com>
2026-03-15 16:51:16 +0000

    fix railway server config

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+
 import App from "./App"
 import "./styles/dashboard.css"
 
-import {BrowserRouter} from "react-router-dom"
-
 ReactDOM.createRoot(document.getElementById("root")).render(
+
   <React.StrictMode>
-    <BrowserRouter>
+
+    <BrowserRouter basename="/dashboard">
+
       <App />
+
     </BrowserRouter>
+
   </React.StrictMode>
+
 )
