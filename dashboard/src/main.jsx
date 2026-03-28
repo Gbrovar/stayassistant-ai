aeb557767306a401ec6a34b38261e0272796c003
gbrovar <gbrovar@gmail.com>
2026-03-28 20:54:02 +0000

    fix railway server config

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-
+import { AppProvider } from "./context/AppContext";
 import App from "./App"
 import "./styles/dashboard.css"
 
@@ -15,11 +15,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
   <React.StrictMode>
 
-    <BrowserRouter basename={basename}>
+    <AppProvider>
 
-      <App />
+      <BrowserRouter basename={basename}>
 
-    </BrowserRouter>
+        <App />
+
+      </BrowserRouter>
+
+    </AppProvider>
 
   </React.StrictMode>
 
