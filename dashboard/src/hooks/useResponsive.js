8ddccfb87ccdf288acfa72ec4c848c4825d51e93
gbrovar <gbrovar@gmail.com>
2026-04-10 22:58:59 +0100

    fix railway server config

--- dashboard/src/hooks/useResponsive.js
+++ dashboard/src/hooks/useResponsive.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 export default function useResponsive() {
 
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
+    const [isMobile, setIsMobile] = useState(false)
 
     useEffect(() => {
 
